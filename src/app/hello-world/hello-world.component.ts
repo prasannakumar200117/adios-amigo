e1835a5df56a24c3fbefc3000a90a65087dcc74f
Priyadharshini19Venkateshan <priyadharshini.venkateshan19@gmail.com>
2024-06-29 09:06:46 +0530

    learnt ngfor and ngclass

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -6,9 +6,48 @@ import { Component } from '@angular/core';
   styleUrl: './hello-world.component.css'
 })
 export class HelloWorldComponent {
-value = '';
-value1 = '';
-handleInput(event:any) {
-this.value = (event.target as HTMLInputElement).value;
-}
+// value = '';
+// value1 = '';
+// handleInput(event:any) {
+// this.value = (event.target as HTMLInputElement).value;
+// }
+
+title = "List of movies ";
+
+movies =  [
+  { 
+    title: 'The Shawshank Redemption',
+    director: 'Frank Darabont',
+    year: 1994,
+    genre: 'Drama'
+  },
+  {
+    title: 'The Godfather',
+    director: 'Francis Ford Coppola',
+    year: 1972,
+    genre: 'Crime'
+  },
+  {
+    title: 'The Dark Knight',
+    director: 'Christopher Nolan',
+    year: 2008,
+    genre: 'Action'
+  },
+  {
+    title: 'Pulp Fiction',
+    director: 'Quentin Tarantino',
+    year: 1994,
+    genre: 'Crime'
+  },
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan',
+    year: 2010,
+    genre: 'Sci-Fi'
+  }
+];
+
+
+
+
 }
